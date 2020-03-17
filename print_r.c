7ef6201a0eccbdaae83804cd1c1f767d6a2140df
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-17 20:38:45 +0000

    hamdi

--- print_r.c
+++ print_r.c
@@ -9,13 +9,16 @@ int print_r(va_list r)
 {
 	char *c;
 	int i = 0, j, len = 0;
+	int a = 0;
 
 	c = va_arg(r, char *);
 	if (c == NULL)
 		c = ")llun(";
-	while (c[i] != '\0')
-		i++;
-	for (j = i; j >= 0; j--)
+
+	for (i = 0; c[i] != '\0'; i++)
+		a = a + 1;
+	a--;
+	for (j = a; j >= 0; j--)
 	{
 		_putchar(c[j]);
 		len++;
