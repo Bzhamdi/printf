415cf9566ee8f68bcd026a2287f71e3afadd7765
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-17 21:34:27 +0000

    betty fix

--- print_unsigned.c
+++ print_unsigned.c
@@ -7,9 +7,9 @@
 */
 int print_u(va_list u)
 {
-	unsigned nb, ddiv, num = 1, len = 0;;
+	unsigned int nb, ddiv, num = 1, len = 0;
 
-	nb = va_arg(u,unsigned int);
+	nb = va_arg(u, unsigned int);
 	ddiv = nb;
 	while ((ddiv / 10) != 0)
 	{
@@ -27,36 +27,38 @@ int print_u(va_list u)
 }
 /**
  * print_x - print on hexa form
- * @n: unsigned int
+ * @x: unsigned int
  * Return: length printed
 */
 int print_x(va_list x)
 {
-        unsigned int nb, quotient, remainder, len = 0;
-        int i, j = 0;
-        char hexadecimalnum[100];
+	unsigned int nb, quotient, remainder, len = 0;
+	int i, j = 0;
+	char hexadecimalnum[100];
 
-        nb = va_arg(x, unsigned int);
-        quotient = nb;
-        while (quotient != 0)
-        {
-                remainder = quotient % 16;
-                if (remainder < 10)
-                        hexadecimalnum[j++] = 48 + remainder;
-                else
-                        hexadecimalnum[j++] = 87 + remainder;
-                        quotient = quotient / 16;
-        }
-        for (i = j - 1; i >= 0; i--)
-        {
-                _putchar(hexadecimalnum[i]);
-                len++;
-        }
-        return (len);
+	nb = va_arg(x, unsigned int);
+	quotient = nb;
+	while (quotient != 0)
+	{
+		remainder = quotient % 16;
+		if (remainder < 10)
+			hexadecimalnum[j++] = 48 + remainder;
+		else
+		{
+			hexadecimalnum[j++] = 87 + remainder;
+			quotient = quotient / 16;
+		}
+	}
+	for (i = j - 1; i >= 0; i--)
+	{
+		_putchar(hexadecimalnum[i]);
+		len++;
+	}
+	return (len);
 }
 /**
  * print_x_upper - print on hexa form
- * @n: unsigned int
+ * @x: unsigned int
  * Return: length printed
 */
 int print_x_upper(va_list x)
@@ -73,8 +75,10 @@ int print_x_upper(va_list x)
 		if (remainder < 10)
 			hexadecimalnum[j++] = 48 + remainder;
 		else
+		{
 			hexadecimalnum[j++] = 55 + remainder;
 			quotient = quotient / 16;
+		}
 	}
 	for (i = j - 1; i >= 0; i--)
 	{
