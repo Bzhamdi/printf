86c148338af42fcea92ed1d9646104a43021e97e
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-03-17 01:10:37 +0000

    hamdi

--- S.c
+++ S.c
@@ -1,10 +1,3 @@
-#include "holberton.h"
-#include <stdlib.h>
-/**
- * print_S - prints the string.
- * @S: unsigned int
- * Return: length printed
- */
 int print_S(va_list S)
 {
 int nb = 0;
@@ -13,21 +6,21 @@ char *str = va_arg(S, char *);
 if (str == NULL)
 str = "(null)";
 
-	while (str[i])
-	{
-		if (str[i] < 32 || str[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			nb += 2;
-			nb += print_x_upper(str[i]);
-		}
-		else
-		{
-			_putchar(str[i]);
-			nb++;
-		}
-		i++;
-	}
-	return (nb);
+        while (str[i])
+        {
+                if (str[i] < 32 || str[i] >= 127)
+                {
+                        _putchar('\\');
+                        _putchar('x');
+                        nb += 2;
+                        nb += print_x_upper(str[i]);
+                }
+                else
+                {
+                        _putchar(str[i]);
+                        nb++;
+                }
+                i++;
+        }
+        return (nb);
 }
